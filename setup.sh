b4ed74038d3eaff36be5ab69b9642b0e5dcc6f81
Nick Jones <nkwjones@gmail.com>
2020-05-10 22:22:16 -0400

    a

--- setup.sh
+++ setup.sh
@@ -1,13 +1,3 @@
 mkdir -p ~/.streamlit/
 
-echo "\
-[general]\n\
-email = \"nkwjones@gmail.com\"\n\
-" > ~/.streamlit/credentials.toml
-
-echo "\
-[server]\n\
-headless = true\n\
-enableCORS=false\n\
-port = $PORT\n\
-" > ~/.streamlit/config.toml
+echo "[server]\n headless = true\n port = $PORT\n enableCORS = false\n\n" > ~/.streamlit/config.toml
